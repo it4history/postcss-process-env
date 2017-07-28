04b15e45d168829989356e9b6ec3b3858d04041e
Віктор Айтішник <it4history@mail.ru>
2017-07-28 12:59:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 var postcss = require('postcss');
 var plugin = require('./package.json');
-
-/**
- * Regex searching for the function-name and its parameters
- */
-var functionRegex = /<%([^<]+)%>/g;
-
 function replaceAll(str, find, replace) {
     return str.replace(new RegExp(find, 'g'), replace);
 }
+
+var functionRegex = /<%([^<]+)%>/g;
+
 /**
  * Parses one CSS-declaration from the given AST and checks for
  * possible replacements
@@ -17,15 +14,15 @@ function replaceAll(str, find, replace) {
  */
 function walkDeclaration(decl) {
     decl.value = decl.value.replace(functionRegex, function (match, value) {
-		value = replaceAll(replaceAll(value,'process.env.','env.') ,'env.','process.env.');
+		var prefix = 'process.env.';
+		value = replaceAll(replaceAll(value, prefix,'env.'), 'env.',prefix);
+		value = replaceAll(value, prefix+'(\\w+)', '(+'+prefix+'$1)'); //converting to number from string
 		return eval(value);
     });
 }
 
 module.exports = postcss.plugin('postcss-env-replace', function (opts) {
     return function (css) {
-        opts = opts || {};
-
         css.walkRules(function walkRule(rule) {
             rule.walkDecls(function (decl) {
                 walkDeclaration(decl);
